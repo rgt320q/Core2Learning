e41920d8bdc6c9ea9c9215ac5937bcb178978fea
rgt320q <omercetin.msn@gmail.com>
2018-07-31 12:29:57 +0300

    Initial commit

---
+++ Core2Learn/Core2LearnWebApp/Data/EntityFramework/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Core2LearnWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2LearnWebApp.Data.EntityFramework
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Rezervation> Rezervations { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
